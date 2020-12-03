e39baf0ff80af036e32239d25993069b045ce6be
Toni Okuogume <toni.okuogume@futurice.com>
2020-12-03 11:10:47 +0200

    Fix file path

--- src/Day1.hs
+++ src/Day1.hs
@@ -2,12 +2,12 @@ module Day1 where
 
 day1_1 :: IO Int
 day1_1 = do
-    input <- lines <$> readFile "~/Haskell/Advent-of-code-2020/input1.txt"
+    input <- lines <$> readFile "input1.txt"
     let numbers = (read :: String -> Int) <$> input
     pure $ uncurry (*) $ head $ filter (\(n1,n2) -> n1 + n2 == 2020) $ [(n1,n2) | n1 <- numbers, n2 <- numbers, n1 /= n2]
 
 day1_2 :: IO Int
 day1_2 = do
-    input <- lines <$> readFile "~/Haskell/Advent-of-code-2020/input1.txt"
+    input <- lines <$> readFile "input1.txt"
     let numbers = (read :: String -> Int) <$> input
     pure $ (\(n1,n2,n3) -> n1 * n2 * n3) $ head $ filter (\(n1,n2,n3) -> n1 + n2 + n3 == 2020) $ [(n1,n2,n3) | n1 <- numbers, n2 <- numbers, n3 <- numbers, n1 /= n2, n2 /= n3, n1 /= n3]
