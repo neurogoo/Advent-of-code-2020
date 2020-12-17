23492fa1d91fc1b7b3383e4d087c3f23b70c9961
Toni Okuogume <toni.okuogume@futurice.com>
2020-12-17 10:19:24 +0200

    Refactoring

--- src/Day17.hs
+++ src/Day17.hs
@@ -3,14 +3,16 @@ module Day17 where
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 
-simulate :: Map.Map (Integer,Integer,Integer) Bool -> Map.Map (Integer, Integer,Integer) Bool
-simulate map' = Map.fromList $ foldr go [] allNeighbours
+neighbours :: (Integer,Integer,Integer) -> [(Integer,Integer,Integer)]
+neighbours (x,y,z) = [(x',y',z')
+                     | x' <- [x-1..x+1]
+                     , y' <- [y-1..y+1]
+                     , z' <- [z-1..z+1]
+                     , (x',y',z') /= (x,y,z)]
+
+simulate :: (Ord a) => (a -> [a]) -> Map.Map a Bool -> Map.Map a Bool
+simulate neighbours map' = Map.fromList $ foldr go [] allNeighbours
   where
-    neighbours (x,y,z) = [(x',y',z')
-                         | x' <- [x-1..x+1]
-                         , y' <- [y-1..y+1]
-                         , z' <- [z-1..z+1]
-                         , (x',y',z') /= (x,y,z)]
     neighbourStates  = map (\k -> Map.findWithDefault False k map') . neighbours
     allNeighbours = Set.fromList $ concatMap neighbours $ Map.keys map'
     go coord acc | Just True <- Map.lookup coord map' =
@@ -31,35 +33,20 @@ day17_1 = do
     let numbered = zip [0..] $ fmap (zip [0..]) input
     let toCoord (y, xs) = fmap (\(x,char) -> if char == '#' then ((x,y,0),True) else ((x,y,0),False)) xs
     let res = Map.fromList $ concat $ toCoord <$> numbered
-    pure $ length $ filter (== True) $ map snd $ Map.toList $ iterate simulate res !! 6
+    pure $ length $ filter (== True) $ map snd $ Map.toList $ iterate (simulate neighbours) res !! 6
 
-hyperSimulate :: Map.Map (Integer,Integer,Integer,Integer) Bool -> Map.Map (Integer, Integer,Integer,Integer) Bool
-hyperSimulate map' = Map.fromList $ foldr go [] allNeighbours
-  where
-    neighbours (x,y,z,w) = [(x',y',z',w')
-                         | x' <- [x-1..x+1]
-                         , y' <- [y-1..y+1]
-                         , z' <- [z-1..z+1]
-                         , w' <- [w-1..w+1]
-                         , (x',y',z',w') /= (x,y,z,w)]
-    neighbourStates  = map (\k -> Map.findWithDefault False k map') . neighbours
-    allNeighbours = Set.fromList $ concatMap neighbours $ Map.keys map'
-    go coord acc | Just True <- Map.lookup coord map' =
-                       let states = length (filter (== True) (neighbourStates coord))
-                       in if states == 2 || states == 3 then
-                         acc <> [(coord, True)]
-                       else
-                         acc <> [(coord, False)]
-    go coord acc = let states = length (filter (== True) (neighbourStates coord))
-                   in if states == 3 then
-                        acc <> [(coord, True)]
-                      else
-                        acc <> [(coord, False)]
+hyperNeighbours :: (Integer,Integer,Integer,Integer) -> [(Integer,Integer,Integer,Integer)]
+hyperNeighbours (x,y,z,w) = [(x',y',z',w')
+                            | x' <- [x-1..x+1]
+                            , y' <- [y-1..y+1]
+                            , z' <- [z-1..z+1]
+                            , w' <- [w-1..w+1]
+                            , (x',y',z',w') /= (x,y,z,w)]
 
 day17_2 :: IO Int
 day17_2 = do
-    input <- lines <$> readFile "src/input17.txt"
+    input <- lines <$> readFile "src/input17test.txt"
     let numbered = zip [0..] $ fmap (zip [0..]) input
     let toCoord (y, xs) = fmap (\(x,char) -> if char == '#' then ((x,y,0,0),True) else ((x,y,0,0),False)) xs
     let res = Map.fromList $ concat $ toCoord <$> numbered
-    pure $ length $ filter (== True) $ map snd $ Map.toList $ iterate hyperSimulate res !! 6
+    pure $ length $ filter (== True) $ map snd $ Map.toList $ iterate (simulate hyperNeighbours) res !! 6
